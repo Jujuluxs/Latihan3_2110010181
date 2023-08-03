dc6fc0d11b27124f57cf277b5a6bb2abeb8edf3e
jujuluxs <sufyannor2016@gmail.com>
2023-08-03 14:45:10 +0800

    membuat class dan menjalankan program

--- src/Geometri/PersegiPanjang.java
+++ src/Geometri/PersegiPanjang.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Geometri;
-
-/**
- *
- * @author USER
- */
 public class PersegiPanjang {
+    //membuat variabel
+    private double panjang,lebar;
     
+    //membuat constructor
+    public PersegiPanjang (double panjang, double lebar){
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+    
+    double setLuas(){
+        return panjang * lebar;
+    }
+    
+    double setKeliling(){
+        return 2*(panjang+lebar);
+    }
 }
+    
+
